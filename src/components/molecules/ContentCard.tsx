b44fb96f445ce5e10f3541544cbc69caf0744750
harunsakin1 <harunsakin16@gmail.com>
2024-11-25 00:39:18 +0300

    Basit bir izleme sayfası tasarlandı.

--- src/components/molecules/ContentCard.tsx
+++ src/components/molecules/ContentCard.tsx
@@ -2,13 +2,18 @@ import React from 'react'
 import CardInfo from './CardInfo'
 import { IContentCard } from '../../models/IContentCard'
 import './ContentCard.css'
+import { useNavigate } from 'react-router-dom';
 
 
 function ContentCard(props: IContentCard) {
+  const navigate=useNavigate();
   const {name,genre,rating, productionYear,imageUrl} = props;
+  const filmIzle=()=>{
+    navigate('/watching');
+  }
   return (
     <div className="col-3 " >
-          <div className='content-card'>
+          <div className='content-card' onClick={filmIzle} >
         <div className="row mt-4 ">
             <img src={imageUrl} alt="film afişi" style={{borderTopLeftRadius: '26px', borderTopRightRadius: '26px'
             }}/>
---
+++ src/models/IWatchingPage.tsx
@@ -0,0 +1,9 @@
+export interface IWatchingPage{
+    name:string,
+    imageUrl:string,
+    videoUrl:string,
+    genre: string,
+    rating: 1|2|3|4|5,
+    productionYear: number,
+    contentDetails: string
+}
